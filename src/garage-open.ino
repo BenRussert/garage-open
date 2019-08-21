b612159d221d64c7e30c67e9c9e8d85dd733a47b
Ben Russert <benrussert@gmail.com>
2019-08-20 23:22:21 -0600

    Cleanup and add webhook

--- src/garage-open.ino
+++ src/garage-open.ino
@@ -1,7 +1,33 @@
-unsigned long duration;
 unsigned long THRESHOLD;
 bool isOpen;
-String status;
+bool wasOpen;
+// String status;
+// String previousStatus;
+inline const char *const boolToString(bool b)
+{
+  return b ? "true" : "false";
+}
+
+void publishStatus(bool shouldPublish)
+{
+  if (shouldPublish)
+  {
+    Particle.publish("isOpen", boolToString(isOpen), 60, PRIVATE);
+  };
+  // reset after publish, but before new measurement
+  wasOpen = isOpen;
+}
+
+bool checkIfOpen(unsigned long threshold = THRESHOLD)
+{
+  digitalWrite(D2, HIGH); // activate trigger
+  delayMicroseconds(10);
+  digitalWrite(D2, LOW); // de-activate trigger
+
+  unsigned long duration = pulseIn(D6, HIGH); // how long until a reply?
+  // open if duration < THRESHOLD, else closed
+  return duration < threshold;
+}
 
 void setup()
 {
@@ -17,35 +43,24 @@ void setup()
   pinMode(D4, OUTPUT);
 
   THRESHOLD = 2000;
-  isOpen = false;
-  status = "Loading";
-  Particle.variable("duration", duration);
-  Particle.variable("garageDoor", status);
+  isOpen = checkIfOpen();
+  publishStatus(true);
   Particle.variable("isOpen", isOpen);
 }
 
 void loop()
 {
+  delay(1000); // even cicuits need a break
 
-  delay(1000);            // even cicuits need a break
-  digitalWrite(D2, HIGH); // activate trigger
-  delayMicroseconds(10);
-  digitalWrite(D2, LOW); // de-activate trigger
-
-  duration = pulseIn(D6, HIGH); // how long until a reply?
-                                // a blocking call so may wait a few seconds
-
-  if (duration < THRESHOLD)
+  if (checkIfOpen())
   {
-    // I'll fix this api later, but isOpen means something is "CLOSE" to the sensor
-    status = "Open";
     isOpen = true;
     digitalWrite(D4, HIGH); // activate trigger
   }
   else
   {
-    status = "Closed";
     isOpen = false;
     digitalWrite(D4, LOW); // activate trigger
   }
+  publishStatus(wasOpen != isOpen);
 }
