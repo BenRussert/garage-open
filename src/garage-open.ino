6ada4723eddd3b289a3b3cb0a377aec616f14d6a
Ben Russert <benrussert@gmail.com>
2019-08-21 00:32:49 -0600

    Something

--- src/garage-open.ino
+++ src/garage-open.ino
@@ -1,3 +1,4 @@
+unsigned long duration;
 unsigned long THRESHOLD;
 bool isOpen;
 bool wasOpen;
@@ -12,7 +13,8 @@ void publishStatus(bool shouldPublish)
 {
   if (shouldPublish)
   {
-    Particle.publish("GARAGE_IS_OPEN", boolToString(isOpen), 60, PRIVATE);
+    // Particle.publish("GARAGE_IS_OPEN", boolToString(isOpen), 60, PRIVATE);
+    Particle.publish("GARAGE_IS_OPEN", String(duration), 60, PRIVATE);
   };
   // reset after publish, but before new measurement
   wasOpen = isOpen;
@@ -24,7 +26,7 @@ bool checkIfOpen(unsigned long threshold = THRESHOLD)
   delayMicroseconds(10);
   digitalWrite(D2, LOW); // de-activate trigger
 
-  unsigned long duration = pulseIn(D6, HIGH); // how long until a reply?
+  duration = pulseIn(D6, HIGH); // how long until a reply?
   // open if duration < THRESHOLD, else closed
   return duration < threshold;
 }
@@ -35,17 +37,18 @@ void setup()
   // ultrasonic range finder Robotshop RB-lte-54
 
   // GND pin goes to ground
-  pinMode(D6, INPUT);  // echo
-  pinMode(D2, OUTPUT); // Trig
+  pinMode(D6, INPUT);  // echo / yellow
+  pinMode(D2, OUTPUT); // Trig / orange
   // VCC pin goes to VIN on the photon
-
+  pinMode(D7, OUTPUT);
   // For LED
-  pinMode(D4, OUTPUT);
-
+  // pinMode(D4, OUTPUT);
+  duration = pulseIn(D6, HIGH); // how long until a reply?
+  Particle.variable("isOpen", isOpen);
+  Particle.variable("duration", duration);
   THRESHOLD = 2000;
   isOpen = checkIfOpen();
   publishStatus(true);
-  Particle.variable("isOpen", isOpen);
 }
 
 void loop()
@@ -55,12 +58,12 @@ void loop()
   if (checkIfOpen())
   {
     isOpen = true;
-    digitalWrite(D4, HIGH); // activate trigger
+    // digitalWrite(D2, HIGH); // activate trigger
   }
   else
   {
     isOpen = false;
-    digitalWrite(D4, LOW); // activate trigger
+    // digitalWrite(D2, LOW); // activate trigger
   }
   publishStatus(wasOpen != isOpen);
 }
